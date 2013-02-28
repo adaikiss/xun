ba9c3ba9d4a4e7985acb1ee827470e5b89b1a303
Hu Lingwei <adaikiss@163.com>
2013-02-28 10:03:36 +0800

    home filter for welcome-file-list

---
+++ jsr/servlet3/src/main/java/org/adaikiss/xun/servlet3/filter/HomeFilter.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.adaikiss.xun.servlet3.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author hlw
+ *
+ */
+@WebFilter(filterName = "HomeFilter", urlPatterns = {"/*"})
+public class HomeFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		String requestURI = req.getRequestURI();
+		if("/".equals(requestURI)){
+			request.getRequestDispatcher("/index.html").forward(request, response);
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
