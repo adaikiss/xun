a45a77bbf042e7ae7ea75954700bc347b08776f4
adaikiss <adaikiss@163.com>
2013-04-08 22:52:11 +0800

    android ListView showcase

--- examples/android/src/main/java/org/adaikiss/xun/android/showcase/ListViewActivity.java
+++ examples/android/src/main/java/org/adaikiss/xun/android/showcase/ListViewActivity.java
@@ -10,7 +10,13 @@ import java.util.Map;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 /**
  * @author hlw
@@ -18,15 +24,36 @@ import android.widget.SimpleAdapter;
  */
 public class ListViewActivity extends ListActivity {
 
+	private List<Map<String, Object>> data;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.list_view);
-		SimpleAdapter adapter = new SimpleAdapter(this, getData(),
+		setContentView(R.layout.list_view);
+		data = getData();
+		final SimpleAdapter adapter = new SimpleAdapter(this, data,
 				R.layout.list_view_item, new String[] { "img", "title",
 						"detail" }, new int[] { R.id.list_view_item_img,
 						R.id.list_view_item_title, R.id.list_view_item_detail });
 		setListAdapter(adapter);
+		Button btn = (Button)findViewById(R.id.list_view_btn);
+		btn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				ListView lv = getListView();
+				StringBuilder sb = new StringBuilder();
+				for(int i = 0; i < lv.getChildCount();i++){
+					View item = lv.getChildAt(i);
+					CheckBox checkBox = (CheckBox)item.findViewById(R.id.list_view_item_checkbox);
+					if(checkBox.isChecked()){
+						sb.append(((Map<String, Object>)adapter.getItem(i)).get("title")).append(" ");
+					}
+				}
+				Toast.makeText(ListViewActivity.this, sb.toString(), Toast.LENGTH_SHORT).show();
+			}
+			
+		});
 	}
 
 	private List<Map<String, Object>> getData() {
